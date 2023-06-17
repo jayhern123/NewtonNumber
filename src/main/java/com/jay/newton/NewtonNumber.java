3603f76ce5ae5da235148a8045b5c257c4346349
jayhern123 <javian587@gmail.com>
2023-06-16 18:15:08 -0700

    better init

--- src/main/java/com/jay/newton/NewtonNumber.java
+++
@@ -1,7 +0,0 @@
-package com.jay.newton;
-
-public class NewtonNumber {
-    public static void main(String[] args) {
-        System.out.println("Initial commit!");
-    }
-}
---
+++ src/main/java/com/jay/newton/NewtonNumberServer.java
@@ -0,0 +1,16 @@
+package com.jay.newton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main app
+ */
+@SpringBootApplication
+public class NewtonNumberServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtonNumberServer.class, args);
+	}
+
+}
